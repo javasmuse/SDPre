bd1d0cdd8a71120637a978ee9e8bb80739cca7f3
javasmuse <candacelewis@mac.com>
2019-06-26 15:34:20 -0600

    the food one is done

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main( String [] args) {
+    String food1, food2, food3;
+    food1 = "tacos";
+    food2 = "BarBQ";
+    food3 = "chiles";
+    System.out.println("My Favorite Foods Are: \n1. " + food1 + "\n2. " + food2 + "\n3. " + food3);
+
+  }
+}
