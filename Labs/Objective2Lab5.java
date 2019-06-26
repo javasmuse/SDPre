2dea77d254fd318834a15ca8d93242d3a32efd65
javasmuse <candacelewis@mac.com>
2019-06-26 16:01:32 -0600

    got the hypotenuse

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,9 @@
+public class Objective2Lab5 {
+  public static void main ( String [] args) {
+  double side1, side2, hypotenuse;
+  side1 = 25;
+  side2 = 16;
+  hypotenuse = Math.sqrt(side1*side1+side2*side2);
+  System.out.println("The hypotenuse of a triangle with sides 25.0 and 16.0 is " +  hypotenuse);
+}
+}
