7743be6b11c87814ee89b8f765505def024d97d6
javasmuse <candacelewis@mac.com>
2019-06-26 13:11:53 -0600

    Exercise 5 done JTHW, with some end of the chapter mods

---
+++ JavaTheHardWay/CreatingVariables.java
@@ -0,0 +1,36 @@
+public class CreatingVariables {
+	public static void main( String[] args ) {
+		int x, y, age, zipcode;
+		double seconds, e, checking, pi;
+		String firstName, lastName, title, dog, bestpal;
+
+		x = 10;
+		y = 400;
+		age = 39;
+		zipcode = 34145;
+
+		seconds = 4.71;
+		e = 2.71828182845904523536;
+		pi = 3.14;
+		checking = 1.89;
+
+		firstName = "Candace";
+		lastName = "Keller";
+		title = "Ms";
+		dog = "Cookie";
+		bestpal = "Paul";
+
+		System.out.println( " " );
+		System.out.println( "The Variable x contains " + x );
+		System.out.println( "The value " + y +" is stored in the variable y." );
+		System.out.println( "The experiment took " + seconds + " seconds." );
+		System.out.println( "My zip code is " +zipcode + " This is for Marco Island, FL " );
+		System.out.println( "A favorite irrational # is Euler's number: " + e );
+		System.out.println( "Two weeks into March we have Pie day! " + pi );
+		System.out.println( "Hopefully you have more than $" + checking + "!" );
+		System.out.println("My name's " + title + " " + firstName + " " + lastName );
+		System.out.println("My dog is " + dog );
+		System.out.println("My best pal is " + bestpal );
+		System.out.println(" " );
+	}
+}
