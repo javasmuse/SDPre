d9c77ebf9c7eda4d4532be40937b96a9b368e886
javasmuse <candacelewis@mac.com>
2019-06-26 15:03:01 -0600

    Obj2Lab2 done

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main( String[] args) {
+    String name = "Candace Lewis";
+    int age = 46;
+    String state = "Georgia";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
