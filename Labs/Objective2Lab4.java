f9a0b1f1221a80fb3cda43346c3270d518228d70
javasmuse <candacelewis@mac.com>
2019-06-26 15:43:17 -0600

    Obj2Lab4 done

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,10 @@
+public class Objective2Lab4 {
+  public static void main( String[] args) {
+    double num1, num2, num3, average;
+    num1 = 13;
+    num2 = 24;
+    num3 = 44;
+    average = (num1+num2+num3)/3;
+    System.out.println(average);
+  }
+}
