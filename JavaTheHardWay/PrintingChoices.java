5d890a2c14edf49784d0d5d4fe1312c43e9063bc
javasmuse <candacelewis@mac.com>
2019-06-26 12:59:03 -0600

    exercise 3 JTHW done

---
+++ JavaTheHardWay/PrintingChoices.java
@@ -0,0 +1,28 @@
+public class PrintingChoices {
+	public static void main( String[] args ) {
+		System.out.println( "Alpha" );
+		System.out.println( "Bravo" );
+
+		System.out.println( "Charlie" );
+		System.out.println( "Delta" );
+		System.out.println();
+
+		System.out.print( "Echo" );
+		System.out.print( "Foxtrot" );
+
+		System.out.println( "Golf" );
+		System.out.print( "Hotel" );
+		System.out.println();
+		System.out.println( "India" );
+
+		System.out.print( "I think I got it ln indicates a single line with return" );
+		System.out.print( "- without the ln it all runs together." );
+		System.out.print( "perhaps?" );
+
+		System.out.println();
+		System.out.println( "This" + " " + "is" + " " + "a" + " test." );
+		System.out.println();
+		System.out.println( "Candace" + " " + "Keller" + " Lewis" );
+	}
+}
+	
