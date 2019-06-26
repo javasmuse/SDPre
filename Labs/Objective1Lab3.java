3227e193955d183f83f2d00f46b21935d14f01e7
javasmuse <candacelewis@mac.com>
2019-06-26 13:38:20 -0600

    Obj1Lab3 done

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main( String [] args) {
+    System.out.println("Name : Candace Lewis");
+    System.out.println("Age : 46");
+    System.out.println("State : Georgia");
+  }
+}
