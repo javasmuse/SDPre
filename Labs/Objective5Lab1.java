067fc9ff9002b55f05ac6cf73211a27b91aaec86
javasmuse <candacelewis@mac.com>
2019-06-27 11:41:39 -0600

    June it is done

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,18 @@
+public class Objective5Lab1 {
+  public static void main( String[] args) {
+    int month = 6;
+
+    switch (month) {
+      case 1:
+        System.out.println("January");
+        break;
+      case 6:
+        System.out.println("June");
+        break;
+        // I could code for each month, but the without an input and the desired output being June, I only did June.
+      default:
+        System.out.println("That is not a valid month");
+          break;
+        }
+      }
+    }
