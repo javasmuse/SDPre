6bd15ef6a609e16635b4527caf27df84d51a0eb4
javasmuse <candacelewis@mac.com>
2019-06-27 14:05:42 -0600

    Obj6 Lab4 done

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,14 @@
+public class Objective6Lab4 {
+  public static void main(String [] args) {
+    int sum, counter;
+    counter = 0;
+    sum = 0;
+
+    while (counter < 20 ) {
+    counter = counter + 1;
+     sum += counter;
+   }
+
+    System.out.println(sum);
+  }
+}
